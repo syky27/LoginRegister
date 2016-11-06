c0cb4de4061ae3ed7a0e949ea1eea80a5efdd2b8
Tomas Sykora, jr <jump@syky.cz>
2016-11-06 21:38:15 +0100

    FEAT: Working prototype

--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -8,62 +8,200 @@
 
 import UIKit
 
+import SkyFloatingLabelTextField
+
+
 class ViewController: UIViewController {
-	var loginSelected = NSLayoutConstraint()
-	var registerSelected = NSLayoutConstraint()
 	let loginView = UIView()
 	let registerView = UIView()
+	let loginLabel = UILabel()
+	let registerLabel = UILabel()
 
-	var labelCenterY = NSLayoutConstraint()
-	var labelCenterX = NSLayoutConstraint()
-	var closedLabel = NSLayoutConstraint()
-	let label = UILabel()
+	var loginSelected = NSLayoutConstraint()
+	var registerSelected = NSLayoutConstraint()
+
+	let imageViewLogin = UIImageView(image: #imageLiteral(resourceName: "optify"))
+	let imageViewRegister = UIImageView(image: #imageLiteral(resourceName: "newbie"))
+
+	let loginUsername = SkyFloatingLabelTextFieldWithIcon()
+	let loginPassword = SkyFloatingLabelTextFieldWithIcon()
+	let registerUsername = SkyFloatingLabelTextFieldWithIcon()
+	let registerPassword = SkyFloatingLabelTextFieldWithIcon()
+
+	let loginButton = UIButton()
+	let registerButton = UIButton()
+
+
+	func setGestureRecognizers() {
+		let registerGesture = UITapGestureRecognizer(target: self, action:  #selector (self.registerTapped))
+		self.registerView.addGestureRecognizer(registerGesture)
+
+		let loginGesture = UITapGestureRecognizer(target: self, action:  #selector (self.loginTapped))
+		self.loginView.addGestureRecognizer(loginGesture)
+	}
+
+	func addSubviews(){
+		loginView.translatesAutoresizingMaskIntoConstraints = false
+		registerView.translatesAutoresizingMaskIntoConstraints = false
+		loginLabel.translatesAutoresizingMaskIntoConstraints = false
+		registerLabel.translatesAutoresizingMaskIntoConstraints = false
+
+		view.addSubview(loginView)
+		view.addSubview(registerView)
+		view.addSubview(loginLabel)
+		view.addSubview(registerLabel)
+	}
+
+
+// func keyboardWillShow(notification: NSNotification) {
+//
+//
+//	view.frame =  CGRect(x: view.frame.origin.x , y: view.frame.origin.y , width: self.view.frame.width, height: self.view.frame.height * 0.5)
+//	self.view.layoutIfNeeded()
+//
+//
+//	}
+//
+//	func keyboardWillHide(notification: NSNotification) {
+//		if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+//			if self.view.frame.origin.y != 0{
+//				self.view.frame.origin.y += keyboardSize.height
+//			}
+//		}
+//	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		loginView.translatesAutoresizingMaskIntoConstraints = false
-		registerView.translatesAutoresizingMaskIntoConstraints = false
-		label.translatesAutoresizingMaskIntoConstraints = false
+//		NotificationCenter.default.addObserver(self,
+//		                                       selector: #selector(ViewController.keyboardWillShow),
+//		                                       name: NSNotification.Name.UIKeyboardWillShow,
+//		                                       object: nil)
+//
+//		NotificationCenter.default.addObserver(self,
+//		                                       selector: #selector(ViewController.keyboardWillHide),
+//		                                       name: NSNotification.Name.UIKeyboardWillHide,
+//		                                       object: nil)
 
-		view.addSubview(loginView)
-		view.addSubview(registerView)
-		view.addSubview(label)
+		let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.extraLight)
+		let blurEffectView = UIVisualEffectView(effect: blurEffect)
+		blurEffectView.frame = view.bounds
+		blurEffectView.alpha = 0.4
+		blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight] // for supporting device rotation
+		view.addSubview(blurEffectView)
+
+		view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "glasses"))
+
+		addSubviews()
+		setGestureRecognizers()
+
+		loginLabel.text = "LOGIN"
+		registerLabel.text = "REGISTER"
+		registerLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+
+		loginView.backgroundColor = UIColor(red: (64/255.0), green: (54/255.0), blue: (105/255.0), alpha: 0.6)
+		registerView.backgroundColor = UIColor(red: (64/255.0), green: (54/255.0), blue: (160/255.0), alpha: 0.7)
 
 
-		label.text = "LOGIN"
-
-		loginView.backgroundColor = .green
-		registerView.backgroundColor = .gray
-
-		let registerGesture = UITapGestureRecognizer(target: self, action:  #selector (self.registerTapped))
-		self.registerView.addGestureRecognizer(registerGesture)
-		let loginGesture = UITapGestureRecognizer(target: self, action:  #selector (self.loginTapped))
-		self.loginView.addGestureRecognizer(loginGesture)
-
-		let verticalConstraint = loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-		let leftConstraint = loginView.leftAnchor.constraint(equalTo: view.leftAnchor)
-		let heightConstraint = loginView.heightAnchor.constraint(equalTo: view.heightAnchor)
-		let rightRegister = registerView.rightAnchor.constraint(equalTo: view.rightAnchor)
-		let heightRegister = registerView.heightAnchor.constraint(equalTo: view.heightAnchor)
-		let leftRegister = registerView.leftAnchor.constraint(equalTo: loginView.rightAnchor)
-
-		labelCenterX = label.centerXAnchor.constraint(equalTo: loginView.centerXAnchor)
-		labelCenterY = label.centerYAnchor.constraint(equalTo: loginView.centerYAnchor)
-		closedLabel = label.rightAnchor.constraint(equalTo: loginView.rightAnchor)
+		loginUsername.setOptify(type: .username)
+		loginPassword.setOptify(type: .password)
+		registerUsername.setOptify(type: .username)
+		registerPassword.setOptify(type: .password)
 
 
-		registerSelected = self.loginView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.1)
+
+		loginView.addSubview(imageViewLogin)
+		registerView.addSubview(imageViewRegister)
+
+		loginView.addSubview(loginUsername)
+		loginView.addSubview(loginPassword)
+		loginView.addSubview(loginButton)
+		registerView.addSubview(registerUsername)
+		registerView.addSubview(registerPassword)
+		registerView.addSubview(registerButton)
+
+
+		imageViewLogin.translatesAutoresizingMaskIntoConstraints = false
+		imageViewLogin.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+		imageViewLogin.centerXAnchor.constraint(equalTo: loginView.centerXAnchor).isActive = true
+		imageViewLogin.widthAnchor.constraint(equalTo: imageViewLogin.heightAnchor).isActive = true
+
+
+		imageViewRegister.translatesAutoresizingMaskIntoConstraints = false
+		imageViewRegister.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+		imageViewRegister.centerXAnchor.constraint(equalTo: registerView.centerXAnchor).isActive = true
+		imageViewRegister.widthAnchor.constraint(equalTo: imageViewRegister.heightAnchor).isActive = true
+
+
+
+
+		loginUsername.topAnchor.constraint(equalTo: imageViewLogin.bottomAnchor, constant: 10).isActive = true
+		loginUsername.centerXAnchor.constraint(equalTo: loginView.centerXAnchor).isActive = true
+
+		loginPassword.topAnchor.constraint(equalTo: loginUsername.bottomAnchor, constant: 20).isActive = true
+		loginPassword.centerXAnchor.constraint(equalTo: loginView.centerXAnchor).isActive = true
+
+		registerUsername.topAnchor.constraint(equalTo: imageViewRegister.bottomAnchor, constant: 10).isActive = true
+		registerUsername.centerXAnchor.constraint(equalTo: registerView.centerXAnchor).isActive = true
+
+		registerPassword.topAnchor.constraint(equalTo: registerUsername.bottomAnchor, constant: 20).isActive = true
+		registerPassword.centerXAnchor.constraint(equalTo: registerView.centerXAnchor).isActive = true
+
+
+
+
+
+
+
+
+		loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+		loginView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+		loginView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+		registerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+		registerView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+		registerView.leftAnchor.constraint(equalTo: loginView.rightAnchor).isActive = true
+
 		loginSelected = loginView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9)
+		registerSelected = loginView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.1)
+
+		loginLabel.centerXAnchor.constraint(equalTo: loginView.centerXAnchor).isActive = true
+		loginLabel.centerYAnchor.constraint(equalTo: loginView.centerYAnchor).isActive = true
+
+		registerLabel.centerXAnchor.constraint(equalTo: registerView.centerXAnchor).isActive = true
+		registerLabel.centerYAnchor.constraint(equalTo: registerView.centerYAnchor).isActive = true
 
 
-		NSLayoutConstraint.activate([verticalConstraint, leftConstraint, heightConstraint, loginSelected,
-		                             rightRegister, heightRegister, leftRegister,
-									labelCenterY, labelCenterX])
+		NSLayoutConstraint.activate([loginSelected])
+
+
+		imageViewRegister.alpha = 0
+		loginLabel.alpha = 0
+		registerUsername.alpha = 0
+		registerPassword.alpha = 0
+		loginLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+		registerLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+		registerButton.alpha = 0
+		registerButton.isEnabled = false
 
 
 
 	}
+
+	override func viewDidLayoutSubviews() {
+		loginButton.setOptify()
+		loginButton.topAnchor.constraint(equalTo: loginPassword.bottomAnchor, constant: 35).isActive = true
+		loginButton.centerXAnchor.constraint(equalTo: loginView.centerXAnchor).isActive = true
+
+		//warning: rewrite to dnamic
+		loginButton.bottomAnchor.constraint(lessThanOrEqualTo: loginView.centerYAnchor).isActive = true
+
+		registerButton.setOptify()
+		registerButton.topAnchor.constraint(equalTo: registerPassword.bottomAnchor, constant: 35).isActive = true
+		registerButton.centerXAnchor.constraint(equalTo: registerView.centerXAnchor).isActive = true
+
+
+	}
+
 
 	func registerTapped() {
 		if !registerSelected.isActive {
@@ -78,26 +216,138 @@ class ViewController: UIViewController {
 	}
 
 	func switchItems()  {
+
+		if self.loginSelected.isActive {
+
+			self.registerLabel.alpha = 0
+		} else {
+			self.loginLabel.alpha = 0
+		}
+
 		UIView.animate(withDuration: 0.75, animations: { _ in
 			if self.loginSelected.isActive {
-				NSLayoutConstraint.deactivate([self.loginSelected, self.labelCenterX])
-				NSLayoutConstraint.activate([self.registerSelected, self.closedLabel])
-				self.label.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+				NSLayoutConstraint.deactivate([self.loginSelected])
+				NSLayoutConstraint.activate([self.registerSelected])
+
+				self.imageViewLogin.alpha = 0
+				self.imageViewRegister.alpha = 1
+				self.loginUsername.alpha = 0
+				self.loginPassword.alpha = 0
+				self.registerUsername.alpha = 1
+				self.registerPassword.alpha = 1
+				self.loginButton.alpha = 0
+				self.loginButton.isEnabled = false
+				self.registerButton.alpha = 1
+				self.registerButton.isEnabled = true
+
 			} else {
-				NSLayoutConstraint.deactivate([self.registerSelected, self.closedLabel])
-				NSLayoutConstraint.activate([self.loginSelected,self.labelCenterX])
-				self.label.transform = CGAffineTransform(rotationAngle: -CGFloat.pi * 2)
+				NSLayoutConstraint.deactivate([self.registerSelected])
+				NSLayoutConstraint.activate([self.loginSelected])
+
+				self.imageViewLogin.alpha = 1
+				self.imageViewRegister.alpha = 0
+				self.loginUsername.alpha = 1
+				self.loginPassword.alpha = 1
+				self.registerUsername.alpha = 0
+				self.registerPassword.alpha = 0
+				self.loginButton.alpha = 1
+				self.loginButton.isEnabled = true
+				self.loginLabel.alpha = 0
+				self.registerButton.alpha = 0
+				self.registerButton.isEnabled = false
+
+
 
 			}
 			self.view.layoutIfNeeded()
-		}, completion: nil)
+		}, completion: { status in
+			if self.loginSelected.isActive {
+				self.loginLabel.alpha = 0
+				self.registerLabel.alpha = 1
+			} else {
+				self.loginLabel.alpha = 1
+				self.registerLabel.alpha = 0
+			}
+
+		})
+
+
+
+
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
-
 
 }
 
+enum SKFTextField {
+	case password
+	case username
+
+}
+
+extension SkyFloatingLabelTextFieldWithIcon {
+	func setOptify(type: SKFTextField) {
+		switch type {
+		case .password:
+			iconText = "\u{f084}"
+			placeholder = "PASSWORD"
+			title = "PASSWORD"
+			isSecureTextEntry = true
+			break
+		case .username:
+			iconText = "\u{f007}"
+			placeholder = "USERNAME"
+			title = "USERNAME"
+			break
+		}
+
+		iconFont = UIFont(name: "FontAwesome", size: 15)
+		selectedIconColor = UIColor.white.withAlphaComponent(0.7)
+		selectedTitleColor = UIColor.white.withAlphaComponent(0.7)
+		textColor = .white
+		selectedLineColor = .white
+		translatesAutoresizingMaskIntoConstraints = false
+		heightAnchor.constraint(equalToConstant: 50).isActive = true
+		widthAnchor.constraint(equalToConstant: 150).isActive = true
+	}
+}
+
+public enum UIButtonBorderSide {
+	case Top, Bottom, Left, Right
+}
+
+
+extension UIButton {
+	func setOptify() {
+	translatesAutoresizingMaskIntoConstraints = false
+	heightAnchor.constraint(equalToConstant: 28).isActive = true
+	widthAnchor.constraint(equalToConstant: 150).isActive = true
+	setTitle("LOGIN", for: .normal)
+	addBorder(side: .Bottom, color: .white, width: 2.0)
+
+	}
+}
+
+extension UIButton {
+
+	public func addBorder(side: UIButtonBorderSide, color: UIColor, width: CGFloat) {
+		let border = CALayer()
+		border.backgroundColor = color.cgColor
+
+		switch side {
+		case .Top:
+			border.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: width)
+		case .Bottom:
+			border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: width)
+		case .Left:
+			border.frame = CGRect(x: 0, y: 0, width: width, height: self.frame.size.height)
+		case .Right:
+			border.frame = CGRect(x: self.frame.size.width - width, y: 0, width: width, height: self.frame.size.height)
+		}
+
+		self.layer.addSublayer(border)
+	}
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LoginScreen' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'SkyFloatingLabelTextField', git: "https://github.com/MLSDev/SkyFloatingLabelTextField.git", branch: "swift3"
+  # Pods for LoginScreen
+
+end
